54424e3b1b1f89ea6b81f2c00b780693b7ebec5a
Whyytas <114874934+Whyytas@users.noreply.github.com>
2024-09-22 23:14:00 +0300

    Add whole project

---
+++ Szofttech-WPF/ViewModel/Commands/SelectItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using Szofttech_WPF.ViewModel;
+using Szofttech_WPF.ViewModel.Base;
+
+namespace Szofttech_WPF.Commands
+{
+    public class SelectItemCommand : CommandBase
+    {
+        public override void Execute(object parameter)
+        {
+            ServerListItemViewModel.SetSelectedServer(parameter);
+        }
+    }
+}
